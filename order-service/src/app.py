9cdf7ea988c5121ae4fd92cd7311b796cb730200
valeriouberti <valerio.uberti@leitha.eu>
2023-08-21 16:10:38 +0200

    Add security to order-service

--- order-service/src/app.py
+++ order-service/src/app.py
@@ -3,7 +3,8 @@ from uuid import uuid4
 import uvicorn
 from confluent_kafka import Producer
 from confluent_kafka.serialization import StringSerializer
-from fastapi import FastAPI
+from fastapi import FastAPI, Security, status, HTTPException, Depends
+from fastapi.security import APIKeyHeader
 
 from model.order import Order
 from settings.settings import settings
@@ -14,8 +15,21 @@ topic = settings.ORDER_TOPIC
 producer_conf = {'bootstrap.servers': settings.BOOTSTRAP_SERVERS}
 string_serializer = StringSerializer('utf_8')
 
+api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
 
-@app.post("/orders")
+
+def get_api_key(
+        api_key_header: str = Security(api_key_header),
+) -> str:
+    if api_key_header in settings.API_KEYS:
+        return api_key_header
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key",
+    )
+
+
+@app.post("/orders", dependencies=[Depends(get_api_key)])
 def create_order(order: Order):
     producer = Producer(producer_conf)
     producer.produce(topic=topic,
--- order-service/src/settings/settings.py
+++ order-service/src/settings/settings.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 from pydantic_settings import BaseSettings
 
@@ -6,6 +7,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     BOOTSTRAP_SERVERS: str = os.environ.get("BOOTSTRAP_SERVERS", "localhost:29092")
     ORDER_TOPIC: str = os.environ.get("ORDER_TOPIC", "orders")
+    API_KEYS: List = os.environ.get("API_KEYS", "changeme").split(",")
 
 
 settings = Settings()
